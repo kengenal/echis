f96974fedb4b440ddefc4a0ef68569fbf692e521
kengenal <kengenal1@gmail.com>
2020-11-16 14:48:40 +0100

    clear

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 from dotenv import load_dotenv
 
 import echis
-from echis import mongo_init
-from echis.model.share import SharedSongs, get_interface
 
 if __name__ == "__main__":
     load_dotenv()
